636d6a75fea06259484f0861878595875ec39d54
mark miranda <notmarkmiranda@gmail.com>
2018-03-14 10:09:46 -0600

    initial

---
+++ app/components/Results.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import queryString from 'query-string'
+import { Link } from 'react-router-dom'
+
+import { battle } from '../utils/api'
+import PlayerPreview from './PlayerPreview'
+import Loading from './Loading'
+
+function Profile({ info }) {
+  return (
+    <PlayerPreview
+      avatar={ info.avatar_url }
+      username={ info.login }>
+      <ul className='space-list-items'>
+        { info.name && <li>{ info.name }</li>}
+        { info.location && <li>{ info.location }</li> }
+        { info.company && <li>{ info.company }</li> }
+        <li>Followers: { info.followers }</li>
+        <li>Following: { info.following }</li>
+        <li>Public Repos: { info.public_repos }</li>
+        { info.blog && <li><a href={ info.blog }>{ info.blog }</a></li> }
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired
+}
+
+function Player({ label, score, profile }) {
+  return(
+    <div>
+      <h1 className='header'>{ label }</h1>
+      <h3 style={{ textAlign: 'center' }}>Score: { score }</h3>
+      <Profile  info={ profile }/>
+    </div>
+  )
+}
+
+Player.propType = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+class Results extends React.Component {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  }
+
+  componentDidMount() {
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search)
+
+    battle([playerOneName, playerTwoName])
+      .then((results) => {
+        if (results === null) {
+          return {
+            error: 'Looks like there was an error. Check that both users exist on Github',
+            loading: false
+          }
+        }
+
+        this.setState(() => ({
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }))
+      })
+  }
+
+  render() {
+    const { error, winner, loser, loading } = this.state
+
+    if (loading === true) {
+      return <Loading />
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{ error }</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
+    return (
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={ winner.score }
+          profile={ winner.profile }
+        />
+        <Player
+          label='Loser'
+          score={ loser.score }
+          profile={ loser.profile }
+        />
+      </div>
+    )
+  }
+}
+
+export default Results
---
+++ app/utils/api.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+
+const id = "YOURCLIENTID"
+const secret = "YOURCLIENTSECRET"
+const params = `?client_id=${id}&client_secret${secret}`
+
+function getProfile(username) {
+  // return axios.get('https://api.github.com/users/' + username + params)
+  return axios.get(`https://api.github.com/users/${username}`)
+    .then(({ data }) => data )
+}
+
+function getRepos(username) {
+  // return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+  return axios.get(`https://api.github.com/users/${username}/repos?per_page=100`)
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function calculateScore({ followers }, repos) {
+  return (followers * 3) + getStarCount(repos)
+}
+
+function handleError(error) {
+  console.warn(error)
+  return null
+}
+
+function getUserData(player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([ profile, repos ]) => ({
+      profile,
+      score: calculateScore(profile, repos)
+  }))
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle (players) {
+  return Promise.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
+}
+
+export function fetchPopularRepos(language) {
+  const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+  return axios.get(encodedURI)
+    .then(({ data }) => data.items )
+}
